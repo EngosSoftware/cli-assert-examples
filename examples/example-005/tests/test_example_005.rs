ad3d28f8a5afac040c216a4e3c99659887f6342e
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-02-03 14:54:24 +0100

    Updates.

--- examples/example-005/tests/test_example_005.rs
+++ examples/example-005/tests/test_example_005.rs
@@ -5,3 +5,11 @@ fn writing_stdin_should_work() {
     .stdout("Hello fron example 005!\n")
     .execute();
 }
+
+#[test]
+fn writing_stdin_again_should_work() {
+  cli_assert::command!()
+    .stdin("Hello fron example 005 once again!\n")
+    .stdout("Hello fron example 005 once again!\n")
+    .execute();
+}
